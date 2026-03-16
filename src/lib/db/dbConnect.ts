129d5863127338a48f8e7e4e4003800a3f478143
dSumitabha <fullstackdev@zanservices.com>
2026-03-16 17:03:22 +0530

    dbConnects file added

---
+++ src/lib/db/dbConnect.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+if (!MONGODB_URI) {
+    throw new Error("Please define MONGODB_URI in .env.local");
+}
+
+interface MongooseCache {
+    conn: typeof mongoose | null;
+    promise: Promise < typeof mongoose > | null;
+}
+
+// @ts-ignore
+let cached: MongooseCache = global.mongoose;
+
+if (!cached) {
+    // @ts-ignore
+    cached = global.mongoose = {
+        conn: null,
+        promise: null
+    };
+}
+
+export default async function dbConnect() {
+    if (cached.conn) return cached.conn;
+
+    if (!cached.promise) {
+        cached.promise = mongoose.connect(MONGODB_URI, {
+            bufferCommands: false,
+        });
+    }
+
+    cached.conn = await cached.promise;
+    return cached.conn;
+}
