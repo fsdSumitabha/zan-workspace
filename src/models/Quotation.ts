2209a2d75811ac3e92fa7b132849dafdd418b1dd
dSumitabha <fullstackdev@zanservices.com>
2026-03-30 20:18:08 +0530

    quotation schema defined

---
+++ src/models/Quotation.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const QuotationSchema = new mongoose.Schema({
+
+    entityType: {
+        type: Number,
+        enum: [0, 1, 2], // 0: LEAD, 1: CLIENT, 2: PROJECT
+        required: true
+    },
+
+    entityId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+
+    title: String,
+
+    amount: {
+        type: Number,
+        required: true
+        //amount will be including gst
+    },
+
+    url: String,
+
+    uploadedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: false
+    }
+
+}, { timestamps: true })
+
+export default mongoose.models.Quotation || mongoose.model("Quotation", QuotationSchema)
