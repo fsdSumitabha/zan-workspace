94d63ca25766784b52f32f37ee0cc9ba8af032f1
sumitabha4 <sumitabha4@gmail.com>
2026-04-19 08:24:47 +0000

    lead create and project create has been updated with the new layout

--- src/app/admin/operations/clients/[clientId]/projects/create/page.tsx
+++ src/app/admin/operations/clients/[clientId]/projects/create/page.tsx
@@ -5,8 +5,6 @@ import { toast } from "sonner"
 import { useRouter, useParams } from "next/navigation"
 import { PROJECT_STATUS } from "@/constants/projectStatus"
 import ClientInfoCard from "@/components/admin/operations/ClientInfoCard"
-import SearchBar from "@/components/admin/operations/SearchBar"
-import StatsPanel from "@/components/admin/operations/StatsPanel"
 
 export default function CreateProjectPage() {
     const router = useRouter()
@@ -79,12 +77,8 @@ export default function CreateProjectPage() {
     }
 
     return (
-        <div className="min-h-screen bg-gray-50 dark:bg-neutral-950 text-gray-900 dark:text-white">
-            <div className="max-w-7xl mx-auto px-4 py-6 grid lg:grid-cols-3 gap-6">
-                <div className="lg:col-span-2 space-y-4">
+            <div className=" space-y-4">
 
-                <SearchBar />
-                {/* Client Context */}
                 <ClientInfoCard />
 
                 {/* Form */}
@@ -182,10 +176,6 @@ export default function CreateProjectPage() {
                         </button>
                     </div>
                 </form>
-                </div>
-                {/* RIGHT */}
-                <StatsPanel />
             </div>
-        </div>
     )
 }
