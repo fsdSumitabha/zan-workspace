806f559297f29a51ce0466cebf157d3d560c232e
Sribabu-Zan <fullstackblockdev@zanservices.com>
2026-06-04 12:32:16 +0530

    added reschedule and cancel and complete options in interactions card

--- src/components/admin/operations/interactions/InteractionItem.tsx
+++ src/components/admin/operations/interactions/InteractionItem.tsx
@@ -5,14 +5,14 @@ import QuotationItem from "./types/Quotationtem"
 import StatusChangeItem from "./types/StatusChangeItem"
 import CallItem from "./types/CallItem"
 
-export default function InteractionItem({ entityType, item }: { entityType: number, item: any;}) {
+export default function InteractionItem({ entityType, item, onChanged }: { entityType: number; item: any; onChanged?: () => void }) {
     switch (item.type) {
         case INTERACTION_TYPE.MEETING_SCHEDULED:
         case INTERACTION_TYPE.MEETING_RESCHEDULED:
         case INTERACTION_TYPE.MEETING_CANCELLED:
         case INTERACTION_TYPE.MEETING_MISSED:
         case INTERACTION_TYPE.MEETING_COMPLETED:
-            return <MeetingItem item={item} />
+            return <MeetingItem item={item} onChanged={onChanged} />
         case INTERACTION_TYPE.NOTE_ADDED:
             return <NoteItem item={item} />
         case INTERACTION_TYPE.QUOTATION_SENT:
